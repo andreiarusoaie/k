64d91f3f36d910d4fe4c2d5eb0cb36208a1a52e5
andreiarusoaie <andrei.arusoaie@gmail.com>
2015-07-12 15:01:47 +0300

    Added abstract backend

--- abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractBackendKompileModule.java
+++ abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractBackendKompileModule.java
@@ -1,7 +1,27 @@
 package org.kframework.backend.abstracT;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.MapBinder;
+import org.kframework.backend.Backend;
+import org.kframework.krun.api.io.FileSystem;
+import org.kframework.krun.ioserver.filesystem.portable.PortableFileSystem;
+
 /**
  * Created by Andrei on 7/12/2015.
  */
-public class AbstractBackendKompileModule {
+public class AbstractBackendKompileModule extends AbstractModule{
+
+
+
+    @Override
+    protected void configure() {
+        bind(FileSystem.class).to(PortableFileSystem.class);
+
+        MapBinder<String, Backend> mapBinder = MapBinder.newMapBinder(
+                binder(), String.class, Backend.class);
+        mapBinder.addBinding("abstract").to(AbstractBackend.class);
+    }
+
 }
+
+
