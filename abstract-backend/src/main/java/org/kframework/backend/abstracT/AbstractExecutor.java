2f63af74d2931ccd06192fa6466cd7411ddf25ec
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2015-07-21 17:02:31 +0200

    Checkin the first version of construct with everything it needs to run on a simple example.

--- abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractExecutor.java
+++ abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractExecutor.java
@@ -2,24 +2,49 @@ package org.kframework.backend.abstracT;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import edu.uci.ics.jung.algorithms.layout.CircleLayout;
-import edu.uci.ics.jung.algorithms.layout.Layout;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import org.kframework.backend.abstracT.graph.AbstractGraph;
+import org.kframework.backend.abstracT.graph.AbstractGraphEdge;
+import org.kframework.backend.abstracT.graph.AbstractGraphNode;
+import org.kframework.backend.abstracT.graph.EdgeType;
+import org.kframework.backend.abstracT.rewrite.engine.AbstractRewriter;
+import org.kframework.backend.abstracT.xml.graph.AbstractGraphNodeSpecification;
+import org.kframework.backend.abstracT.xml.graph.AbstractGraphSpecification;
+import org.kframework.backend.java.kil.ConstrainedTerm;
 import org.kframework.backend.java.kil.Definition;
 import org.kframework.backend.java.kil.GlobalContext;
+import org.kframework.backend.java.kil.TermContext;
+import org.kframework.backend.java.kil.Variable;
+import org.kframework.backend.java.symbolic.BackendJavaKILtoKILTransformer;
+import org.kframework.backend.java.symbolic.ConjunctiveFormula;
+import org.kframework.backend.java.symbolic.CopyOnWriteTransformer;
+import org.kframework.backend.java.symbolic.Equality;
 import org.kframework.backend.java.symbolic.JavaExecutionOptions;
 import org.kframework.backend.java.symbolic.JavaSymbolicExecutor;
 import org.kframework.backend.java.symbolic.KILtoBackendJavaKILTransformer;
 import org.kframework.backend.java.symbolic.PatternMatchRewriter;
+import org.kframework.backend.java.symbolic.PersistentUniqueList;
+import org.kframework.backend.java.symbolic.Substitution;
 import org.kframework.backend.java.symbolic.SymbolicRewriter;
+import org.kframework.compile.utils.RuleCompilerSteps;
+import org.kframework.kil.ASTNode;
+import org.kframework.kil.BoolBuiltin;
+import org.kframework.kil.Rule;
+import org.kframework.kil.Term;
 import org.kframework.krun.KRunExecutionException;
-import org.kframework.krun.api.KRunGraph;
 import org.kframework.krun.api.KRunState;
 import org.kframework.krun.api.RewriteRelation;
+import org.kframework.krun.api.SearchResults;
+import org.kframework.krun.api.SearchType;
+import org.kframework.parser.ProgramLoader;
 import org.kframework.utils.Stopwatch;
 
-import javax.swing.*;
-import java.awt.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by andrei on 14.07.2015.
@@ -27,32 +52,222 @@ import java.awt.*;
 public class AbstractExecutor extends JavaSymbolicExecutor {
 
     private final AbstractOptions abstractOptions;
+    private final Provider<ProgramLoader> programLoader;
 
     @Inject
-    AbstractExecutor(org.kframework.kil.loader.Context context, JavaExecutionOptions javaOptions, KILtoBackendJavaKILTransformer kilTransformer, GlobalContext globalContext, Provider<SymbolicRewriter> symbolicRewriter, Provider<PatternMatchRewriter> patternMatchRewriter, KILtoBackendJavaKILTransformer transformer, Definition definition, KRunState.Counter counter, Stopwatch sw, AbstractOptions abstractOptions) {
+    AbstractExecutor(org.kframework.kil.loader.Context context, JavaExecutionOptions javaOptions, KILtoBackendJavaKILTransformer kilTransformer, GlobalContext globalContext, Provider<SymbolicRewriter> symbolicRewriter, Provider<PatternMatchRewriter> patternMatchRewriter, KILtoBackendJavaKILTransformer transformer, Definition definition, KRunState.Counter counter, Stopwatch sw, AbstractOptions abstractOptions, Provider<ProgramLoader> programLoader) {
         super(context, javaOptions, kilTransformer, globalContext, symbolicRewriter, patternMatchRewriter, transformer, definition, counter, sw);
         this.abstractOptions = abstractOptions;
+        this.programLoader = programLoader;
     }
+
 
     @Override
     public RewriteRelation run(org.kframework.kil.Term cfg, boolean computeGraph) throws KRunExecutionException {
-        RewriteRelation rewriteRelation = javaRewriteEngineRun(cfg, -1, true);
-        KRunGraph graph = rewriteRelation.getExecutionGraph().get();
+        AbstractGraphSpecification abstractGraph = new AbstractGraphSpecification(abstractOptions.abstractGraph, programLoader, getContext());
+        RewriteRelation temp = javaRewriteEngineRun((Term) cfg, 152, true);
+        return temp;
+    }
 
-        Layout<KRunState, String> layout = new CircleLayout(graph);
-        layout.setSize(new Dimension(1000,1000)); // sets the initial size of the space
-        // The BasicVisualizationServer<V,E> is parameterized by the edge types
-        BasicVisualizationServer<KRunState,String> vv =
-                new BasicVisualizationServer<KRunState,String>(layout);
-        vv.setPreferredSize(new Dimension(1050,1050)); //Sets the viewing area size
+    @Override
+    public SearchResults search(Integer bound, Integer depth, SearchType searchType, Rule pattern, Term cfg, RuleCompilerSteps compilationInfo, boolean computeGraph) throws KRunExecutionException {
+        AbstractGraphSpecification abstractGraphSpecification = new AbstractGraphSpecification(abstractOptions.abstractGraph, programLoader, getContext());
+        Map.Entry<ConstrainedTerm, ConstrainedTerm> mainGoal = getMainFormula(abstractGraphSpecification);
+        List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> G = getListOfGoals(abstractGraphSpecification);
+        AbstractGraph abstractGraph = getGraph(mainGoal, G, pattern);
+        abstractGraph.displayGraph();
+        new Scanner(System.in).nextLine();
+        return super.search(bound, depth, searchType, pattern, cfg, compilationInfo, computeGraph);
+    }
 
-        JFrame frame = new JFrame("Simple Graph View");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(vv);
-        frame.pack();
-        frame.setVisible(true);
+    // this method is 'hacky' since it returns the first formula as the main formula
+    // TODO: fix the way we identify the main formula
+    private Map.Entry<ConstrainedTerm, ConstrainedTerm> getMainFormula(AbstractGraphSpecification abstractGraphSpecification) {
+        AbstractGraphNodeSpecification abstractGraphNodeSpecification = abstractGraphSpecification.getAbstractGraphNodeSpecs().get(0);
+        return getConstrainedFormula(abstractGraphNodeSpecification);
+    }
 
-        return rewriteRelation;
+
+    private List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> getListOfGoals(AbstractGraphSpecification abstractGraphSpecification) {
+        List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> goals = new ArrayList<>();
+        for (AbstractGraphNodeSpecification abstractGraphNodeSpecification : abstractGraphSpecification.getAbstractGraphNodeSpecs()) {
+            goals.add(new AbstractMap.SimpleEntry<ConstrainedTerm, ConstrainedTerm>(getConstrainedFormula(abstractGraphNodeSpecification)));
+        }
+        return goals;
+    }
+
+    private Map.Entry<ConstrainedTerm, ConstrainedTerm> getConstrainedFormula(AbstractGraphNodeSpecification abstractGraphNodeSpecification){
+        TermContext termContext = TermContext.of(getGlobalContext());
+
+        org.kframework.backend.java.kil.Term lhsTerm = getKilTransformer().transformAndEval(abstractGraphNodeSpecification.getLhs());
+        org.kframework.backend.java.kil.Term lhsConstraint = getKilTransformer().transformAndEval(abstractGraphNodeSpecification.getLhsConstraint());
+        ConstrainedTerm lhs = getConstrainedTerm(lhsTerm, lhsConstraint, termContext);
+
+        org.kframework.backend.java.kil.Term rhsTerm = getKilTransformer().transformAndEval(abstractGraphNodeSpecification.getRhs());
+        org.kframework.backend.java.kil.Term rhsConstraint = getKilTransformer().transformAndEval(abstractGraphNodeSpecification.getRhsConstraint());
+        ConstrainedTerm rhs = getConstrainedTerm(rhsTerm, rhsConstraint, termContext);
+
+        return new AbstractMap.SimpleEntry<ConstrainedTerm, ConstrainedTerm>(lhs, rhs);
+    }
+
+    private AbstractGraph getGraph(
+            Map.Entry<ConstrainedTerm, ConstrainedTerm> main,
+            List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> G,
+            Rule pattern) throws KRunExecutionException {
+
+        // compute the first set of derivatives and then process each of them
+        List<ConstrainedTerm> derivatives = AbstractRewriter.oneSearchStep(main.getKey(), pattern, getGlobalContext(), getSymbolicRewriter(), getContext(), getTransformer());
+
+        AbstractGraph graph = AbstractGraph.empty();
+        AbstractGraphNode root = new AbstractGraphNode(main.getKey(), main.getValue());
+        graph.addNode(root);
+        for (ConstrainedTerm derivative : derivatives) {
+            AbstractGraphNode child = new AbstractGraphNode(derivative, main.getValue());
+            graph.addNode(child);
+            graph.addEdge(new AbstractGraphEdge(root, child, EdgeType.SYMBOLIC_STEP));
+            graph = construct(new AbstractMap.SimpleEntry<>(derivative, main.getValue()), G, pattern, graph);
+        }
+        return graph;
+    }
+
+
+    private AbstractGraph construct(
+            Map.Entry<ConstrainedTerm, ConstrainedTerm> formula,
+            List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> G,
+            Rule pattern,
+            AbstractGraph graph
+            ) throws KRunExecutionException {
+
+        if (formula.getKey().implies(formula.getValue())) {
+            return graph;
+        }
+        else {
+            Map.Entry<ConstrainedTerm, ConstrainedTerm> circ = searchCircularity(formula.getKey(), G);
+            if (circ != null) {
+                AbstractGraphNode source = graph.getNode(formula.getKey(), formula.getValue());
+                AbstractGraphNode circNode = new AbstractGraphNode(circ.getKey(), formula.getValue());
+
+                if (graph.hasNode(circNode)) {
+                    List<AbstractGraphNode> circDerivatives = graph.getSuccesorsByEdgeType(circNode, EdgeType.SYMBOLIC_STEP);
+                    for (AbstractGraphNode circDerivative : circDerivatives) {
+                        graph.addEdge(new AbstractGraphEdge(source, circDerivative, EdgeType.CIRCULARITY));
+                    }
+                }
+                else {
+                    // add circ in the graph in order to detect if it was processed at previous step
+                    graph.addNode(circNode);
+
+                    List<ConstrainedTerm> circDerivatives = AbstractRewriter.oneSearchStep(circ.getKey(), pattern, getGlobalContext(), getSymbolicRewriter(), getContext(), getTransformer());
+                    for (ConstrainedTerm circDerivative : circDerivatives) {
+                        // build a new node and add it to graph
+                        AbstractGraphNode targetNode = new AbstractGraphNode(circDerivative, formula.getValue());
+                        graph.addNode(targetNode);
+
+                        // add symb step from circ to circ der (internal stuff)
+                        graph.addEdge(new AbstractGraphEdge(circNode, targetNode, EdgeType.SYMBOLIC_STEP));
+
+                        // add symb step from formula to circ der
+                        graph.addEdge(new AbstractGraphEdge(source, targetNode, EdgeType.CIRCULARITY));
+
+                        // call construct over circ derivatives
+                        graph = construct(new AbstractMap.SimpleEntry<>(circDerivative, formula.getValue()), G, pattern, graph);
+                    }
+                }
+            }
+            else {
+                AbstractGraphNode toExpand = new AbstractGraphNode(formula.getKey(), formula.getValue());
+                AbstractGraph graphToExpand = AbstractGraph.empty();
+                graphToExpand.addNode(toExpand);
+                AbstractGraph symbolicGraph = expand(graphToExpand, G, pattern, 10);
+                graph.addSubgraph(symbolicGraph);
+
+                if (!symbolicGraph.isSingletonGraph(toExpand)) {
+                    List<AbstractGraphNode> frontier = symbolicGraph.getFrontier();
+                    for (AbstractGraphNode toProcess : frontier) {
+                        graph = construct(new AbstractMap.SimpleEntry<ConstrainedTerm, ConstrainedTerm>(toProcess.getLhs(), formula.getValue()), G, pattern, graph);
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    private AbstractGraph expand(
+            AbstractGraph graph,
+            List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> G,
+            Rule pattern,
+            int maxDepth) throws KRunExecutionException {
+
+        while (maxDepth > 0) {
+            List<AbstractGraphNode> frontier = graph.getFrontier();
+            AbstractGraphNode toProcess = null;
+            List<ConstrainedTerm> derivatives = null;
+            for (AbstractGraphNode abstractGraphNode : frontier) {
+                List<ConstrainedTerm> cDerivatives = AbstractRewriter.oneSearchStep(abstractGraphNode.getLhs(), pattern, getGlobalContext(), getSymbolicRewriter(), getContext(), getTransformer());
+                if (!abstractGraphNode.getLhs().implies(abstractGraphNode.getRhs()) &&
+                        searchCircularity(abstractGraphNode.getLhs(), G) == null &&
+                        !cDerivatives.isEmpty()) {
+                    toProcess = abstractGraphNode;
+                    derivatives = cDerivatives;
+                    break;
+                }
+            }
+            if (toProcess == null) {
+                break;
+            } else {
+                for (ConstrainedTerm d : derivatives) {
+                    AbstractGraphNode successor = new AbstractGraphNode(d, toProcess.getRhs());
+                    graph.addNode(successor);
+                    graph.addEdge(new AbstractGraphEdge(toProcess, successor, EdgeType.SYMBOLIC_STEP));
+                }
+            }
+            maxDepth--;
+        }
+
+        return graph;
+    }
+
+
+
+
+    private Map.Entry<ConstrainedTerm,ConstrainedTerm> searchCircularity(ConstrainedTerm phi, List<Map.Entry<ConstrainedTerm, ConstrainedTerm>> G) {
+        for (Map.Entry<ConstrainedTerm, ConstrainedTerm> c : G) {
+            if (phi.implies(replaceVariablesWithFresh(c.getKey()))) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+    private ConstrainedTerm replaceVariablesWithFresh(ConstrainedTerm term) {
+        return (ConstrainedTerm) term.accept(new CopyOnWriteTransformer(term.termContext()) {
+            @Override
+            public ASTNode transform(Variable variable) {
+                return new Variable(variable.name().concat("gen"), variable.sort());
+            }
+        });
+    }
+
+    private Term toKIL(org.kframework.backend.java.kil.Term javaTerm) {
+        return (Term) javaTerm.accept(new BackendJavaKILtoKILTransformer(getContext()));
+    }
+
+    private org.kframework.backend.java.kil.Term toJavaKIL(Term term) {
+        org.kframework.backend.java.kil.Term javaTerm = getKilTransformer().transformAndEval(term);
+        TermContext termContext = TermContext.of(getGlobalContext());
+        termContext.setTopTerm(javaTerm);
+        return javaTerm;
+    }
+
+    private ConstrainedTerm getConstrainedTerm(org.kframework.backend.java.kil.Term initialTerm, org.kframework.backend.java.kil.Term initialConstraint, TermContext termContext) {
+        // prepare the left hand side
+        Equality equality = new Equality(initialConstraint, getKilTransformer().transformAndEval(BoolBuiltin.TRUE), termContext);
+        Collection<Equality> equalityCollection = new LinkedList<>();
+        equalityCollection.add(equality);
+        PersistentUniqueList<Equality> persistentUniqueList = PersistentUniqueList.from(equalityCollection);
+        return new ConstrainedTerm(initialTerm, ConjunctiveFormula.of(Substitution.empty(), persistentUniqueList, PersistentUniqueList.empty(), termContext));
     }
 
 }
--- abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractKRunModule.java
+++ abstract-backend/src/main/java/org/kframework/backend/abstracT/AbstractKRunModule.java
@@ -1,42 +1,9 @@
 package org.kframework.backend.abstracT;
 
-import com.beust.jcommander.JCommander;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
-import com.google.inject.multibindings.MapBinder;
 import com.google.inject.multibindings.Multibinder;
-import org.kframework.Rewriter;
-import org.kframework.backend.java.kil.Definition;
-import org.kframework.backend.java.kil.GlobalContext;
-import org.kframework.backend.java.ksimulation.Simulator;
-import org.kframework.backend.java.symbolic.FreshRules;
-import org.kframework.backend.java.symbolic.InitializeRewriter;
-import org.kframework.backend.java.symbolic.JavaSymbolicCommonModule;
-import org.kframework.backend.java.symbolic.JavaSymbolicExecutor;
-import org.kframework.backend.java.symbolic.Stage;
-import org.kframework.backend.java.symbolic.SymbolicRewriter;
-import org.kframework.definition.Module;
-import org.kframework.kil.loader.Context;
-import org.kframework.krun.KRunOptions;
-import org.kframework.krun.api.KRunResult;
-import org.kframework.krun.tools.Executor;
-import org.kframework.main.AnnotatedByDefinitionModule;
-import org.kframework.transformation.ToolActivation;
-import org.kframework.transformation.Transformation;
-import org.kframework.utils.BinaryLoader;
-import org.kframework.utils.Stopwatch;
-import org.kframework.utils.errorsystem.KExceptionManager;
-import org.kframework.utils.file.FileUtil;
-import org.kframework.utils.inject.Annotations;
-import org.kframework.utils.inject.DefinitionScoped;
-import org.kframework.utils.inject.Main;
 import org.kframework.utils.inject.Options;
-import org.kframework.utils.inject.RequestScoped;
-import org.kframework.utils.inject.Spec;
-
-import java.util.List;
-import java.util.function.Function;
 
 /**
  * Created by andrei on 14.07.2015.
@@ -58,5 +25,4 @@ public class AbstractKRunModule extends AbstractModule {
         Multibinder<Class<?>> experimentalOptionsBinder = Multibinder.newSetBinder(binder(), new TypeLiteral<Class<?>>() {}, Options.class);
         experimentalOptionsBinder.addBinding().toInstance(AbstractOptions.class);
     }
-
 }
